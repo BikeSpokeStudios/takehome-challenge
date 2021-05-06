5f93975fa27a403446a13a302f2050e2d203b78a
David Parker <dav1dp0101@1022606.no-reply.drupal.org>
2021-05-06 15:12:22 -0400

    Finished the challenge and added README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Take Home Challenge  
+
+This is my solution to a take home challenge I was asked to complete. I was provided a screenshot and asked to replicate the design using the CSS Vanilla Framework.
+
+* [Vanilla Framework](https://vanillaframework.io/)
+* [NPM](https://www.npmjs.com/)
+* [Dart Sass](https://sass-lang.com/dart-sass)
+* [Webpack](https://webpack.js.org/)
+* [Wordpress API](https://developer.wordpress.org/rest-api/)
+
+The Vanilla framework is an alternative to Bootstrap that allows for rapid prototyping and creating good-looking websites. It employs some nice typefaces.
+
+I pulled data from the Wordpress API to get three blog posts. I then styled them using Vanilla's provided class names. I had to write a small amount of CSS to achieve the design. The Card class in Vanilla doesn't have the coloured strip across the top but the Notification class does, so I made my cards a hybrid of the two types and wrote two lines of CSS to fix some of the styles.
+
+* [Repo URL]()
+* [Live Page]()
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,32 +2,43 @@ import $ from 'jquery';
 import axios from 'axios';
 
 function renderCard(post) {
+    let date = new Date(post.date);
+    let options = { month: 'short'};
     return `
         <div class="col-4 p-card p-notification--information blog-card">
-            <div class="">
-                <h4>${(post._embedded["wp:term"][1][0].name || " ").toUpperCase()}</h4>
+            <div>
+                <h4 class="tag-name">${(post._embedded["wp:term"][1][0].name || " ").toUpperCase()}</h4>
                 <hr>
                 <img src="${post.featured_media}">
                 <h3 class="p-card__content">
                     <a href="${post.link}">${post.title.rendered}</a>
                 </h3>
-                <p class="p-heading--6">By ${} on ${}</p>
+                <p class="p-heading--6">By <a 
+                    href="${post._embedded.author[0].link || " "}">${post._embedded.author[0].name || " "}</a> on 
+                    ${date.getDate()} ${new Intl.DateTimeFormat('en-US', options).format(date)} ${date.getFullYear()}
+                </p>
             </div>
+            <div class="card-bottom">
+                    <hr>
+                    <h4>${post._embedded["wp:term"][0][0].name || " "}</h4>
+                </div>
         </div>`;
 }
 
 function render(data) {
-    console.log("This is data parameter in render:", data);
     $('#results').html(data.map(post => renderCard(post)).join(''));
 }
 
+function getData() {
+    axios.get('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json')
+    .then(responseJson => {
+        render(responseJson.data);
+    })
+    .catch(err => {
+            console.log("Something went wrong. Error: ", err);
+    });
+}
 
-axios.get('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json')
-.then(responseJson => {
-    render(responseJson.data);
-})
-.catch(err => {
-        console.log("Something went wrong. Error: ", err);
-});
+$(getData);
 
 
